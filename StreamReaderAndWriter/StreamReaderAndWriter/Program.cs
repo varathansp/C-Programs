3faedd68b57a74a38bd0940bb081c9cebee63612
Varathan <varathansuyambu95@gmail.com>
2018-08-29 11:41:53 +0530

    StreamReader Writer Program added

---
+++ StreamReaderAndWriter/StreamReaderAndWriter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StreamReaderAndWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"C:\Sample Files\Log.txt");
+            Console.Write(reader.ReadToEnd());
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(@"C:\Sample Files\Log.txt");
+            writer.Write("This is written by StreamWriter");
+            writer.Close();
+        
+        }
+    }
+}

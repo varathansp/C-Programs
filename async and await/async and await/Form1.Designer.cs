c05fd1c4b0dcbde72056567bf2cd0d6dca61ecf1
varathan <varathansuyambu95@gmail.com>
2018-09-10 23:38:25 +0530

    Async Await program added

---
+++ async and await/async and await/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace async_and_await
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCount = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(72, 28);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 0;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(96, 78);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ async and await/async and await/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace async_and_await
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double countChar()
+        {
+            double count = 0;
+            using (StreamReader reader = new StreamReader("C:\\data\\data.txt"))
+            {
+                string charr = reader.ReadToEnd();
+                count = charr.Length;
+                Thread.Sleep(5000);
+            }
+            return count; 
+        }
+        private async void btnCount_Click(object sender, EventArgs e)
+        {
+            Task<double> taskCount = new Task<double>(countChar);
+            taskCount.Start();
+            lblMessage.Text = "Count is on Process..please wait..";
+            double count = await taskCount;
+            lblMessage.Text = "Character count is "+count;
+        }
+    }
+}
